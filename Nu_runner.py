eb68b2c1d1f7dcb24c86ab6478d4521d663b2092
Samuel Witte <witte.sam@gmail.com>
2017-04-24 13:28:20 -0700

    forgot ars term

--- Nu_runner.py
+++ Nu_runner.py
@@ -45,5 +45,5 @@ elif args.GF == 'F':
 nu_floor(args.sig_low, args.sig_high, n_sigs=args.n_sigs, model=args.model,
          mass=args.mass, fnfp=args.fnfp, element=args.element, exposure=args.exposure,
          delta=args.delta, GF=False, time_info=time, file_tag=args.file_tag, n_runs=args.n_runs,
-         Eth=e_th)
+         Eth=args.e_th)
 
