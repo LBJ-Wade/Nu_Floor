503ae94845cb88d03cb8aaad72b826b48bd1e60e
Samuel Witte <witte.sam@gmail.com>
2017-04-24 21:40:15 -0700

    trying something

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 from scipy.interpolate import interp1d
-from scipy.special import erf
+from scipy.special import erfc,erf
 from scipy.optimize import brentq, curve_fit
 from scipy.stats import norm
 import numpy.random as random
@@ -9,7 +9,8 @@ import numpy.random as random
 import warnings
 from scipy.optimize import OptimizeWarning
 
-warnings.simplefilter("ignore", OptimizeWarning)
+warnings.simplefilter("error", OptimizeWarning)
+warnings.simplefilter("error", RuntimeError)
 
 path = os.getcwd()
 
@@ -335,23 +336,35 @@ def adaptive_samples(sig_min, sig_max, list):
     elif high == 0:
         return np.mean(np.array([np.max(arr_l[:, 0]), sig_max])), False
     else:
-
-        mean = sum(arr_l[:,0] * arr_l[:,1]) / sum(arr_l[:,1])
-        popt, pcov = curve_fit(gauss_cdf_function, arr_l[:,0], arr_l[:,1], p0=[mean, 1.],
-                               bounds=([arr_l[0, 0], arr_l[-1, 0]], [1e-4, 10.]))
-        xrag = np.linspace(sig_min, sig_max, 300)
-        ypts = gauss_cdf_function(xrag, *popt)
-        ypts /= ypts.max()
-        u = random.rand(1)
-        sig = xrag[np.absolute(ypts - u).argmin()]
-        # else:
-        #     low_v = np.max(arr_l[arr_l[:, 1] == 0.][:, 0])
-        #     high_v = np.min(arr_l[arr_l[:, 1] == 1.][:, 0])
-        #     sig = np.mean(np.array([low_v, high_v]))
+        try:
+            mean = sum(arr_l[:,0] * arr_l[:,1]) / sum(arr_l[:,1])
+            popt, pcov = curve_fit(gauss_cdf_function, arr_l[:,0], arr_l[:,1], p0=[mean, 0.5],
+                                   bounds=([sig_min,sig_max], [0.01, 10.]))
+            xrag = np.linspace(sig_min, sig_max, 300)
+            ypts = gauss_cdf_function(xrag, *popt)
+            ypts /= ypts.max()
+            u = random.rand(1)
+            sig = xrag[np.absolute(ypts - u).argmin()]
+        except OptimizeWarning or RuntimeError:
+            print 'Optimize failed.'
+            sig = guess_x0(sig_min, sig_max, arr_l)
     return sig, False
 
 
 def gauss_cdf_function(x, x0, sigma):
-    z = (x - x0) / (np.sqrt(2.) * sigma)
-    return (np.pi / 2.) * sigma * (erf(z) + 1.)
+    z = (x0 - x) / (np.sqrt(2.) * sigma)
+    return 0.5*erfc(z)
+
+def guess_x0(sig_min, sig_max, data):
+    #print 'trying...', sig_min, sig_max, data
+    x0 = np.linspace(sig_min, sig_max, 50)
+    looker = np.zeros_like(x0)
+    for j in range(len(x0)):
+        for i in range(len(data[:,0])):
+            looker[j] = (gauss_cdf_function(data[i,0], x0[j], 0.05) - data[i,1])**2. / (data[i,1] + 0.1)**2.
+
+    looker[looker == 0.] = 1.
+    #print np.column_stack((x0, looker))
+    sig = x0[np.argmin(looker)]
+    return sig
 
