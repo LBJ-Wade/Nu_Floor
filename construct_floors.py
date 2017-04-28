7cba95795c2bfc83443fe3eddab5b96a86964dbd
Samuel Witte <witte.sam@gmail.com>
2017-04-28 10:34:36 -0700

    ok hopefully good

--- construct_floors.py
+++ construct_floors.py
@@ -49,10 +49,10 @@ def make_a_floor(element='Germanium', model='sigma_si', fnfp=1., exposure=1.,
             rm_ind = [idx for idx, item in enumerate(load[:,0]) if item in load[:,0][:idx]]
             useable = np.delete(load, rm_ind, axis=0)
             try:
-                mean = sum(useable[:, 0] * useable[:, 1]) / sum(useable[:, 1])
-                popt, pcov = curve_fit(gauss_cdf_function, useable[:, 0], useable[:, 1], p0=[mean, 1.])
-                csec = brentq(lambda x: gauss_cdf_function(x, *popt) - qaim, -60., -30.)
-#                csec = brentq(lambda x: interp1d(useable[:,0], useable[:,1])(x) - qaim, np.min([useable[:,0]]), np.max(useable[:,0]))
+#                mean = sum(useable[:, 0] * useable[:, 1]) / sum(useable[:, 1])
+#                popt, pcov = curve_fit(gauss_cdf_function, useable[:, 0], useable[:, 1], p0=[mean, 1.])
+#                csec = brentq(lambda x: gauss_cdf_function(x, *popt) - qaim, -60., -30.)
+                csec = brentq(lambda x: interp1d(useable[:,0], useable[:,1])(x) - qaim, np.min([useable[:,0]]), np.max(useable[:,0]))
                 print 'DM mass: {:.2f}, Cross Sec {:.2e}'.format(mx, 10. ** csec)
 #                print 'Array: ', useable
 #                print 'Best Fit: ', popt
